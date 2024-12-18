070a4133555321c90bbd91c12e6fa328c17c32ce
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2024-12-18 13:13:07 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.57 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,35 +3,36 @@ class Solution {
     public int solution(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
-        boolean[][] visited = new boolean[n][m];
-        int answer = 0;
+        boolean[][] visited = new boolean[n][m]; //방문 처리를 위해 boolean 
+        Queue<int[]> queue = new LinkedList<>(); // 모든 경우의 수를 돌기위해 queue 사용
+        
         int[] dx = {0,0,-1,1};
         int[] dy = {-1,1,0,0};
-        Queue<int[]> queue = new LinkedList<>(); 
-        queue.offer(new int[]{0,0,1});
         
-        while(!queue.isEmpty()){
-            int[] current = queue.poll();
+        
+        queue.offer(new int[]{0,0,1}); //초기 값 세팅
+        visited[0][0] = true;
+        
+        while(!queue.isEmpty()){ // queue가 채워져있는동안 돌기, 만약 queue가 비워졌는데 목표하는 곳에 도달하지 못하면 
+            int[] current = queue.poll();// return값이 없으므로 외부에서 처리
             int x = current[0];
             int y = current[1];
-            int distance  = current[2];
+            int distance  = current[2]; // queue에서 하나 꺼내서 값 정리
             
             
-            if(x == n-1&& y == m-1){
+            if(x == n-1&& y == m-1){ //목표에 도달하면 최단거리 값 출력
                 return distance;
             }
             
-            for(int i = 0; i < 4; i++){
+            for(int i = 0; i < 4; i++){ //4방향을 돌면서
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
-                if(nx >= 0&& nx < n && ny >=0 && ny < m && !visited[nx][ny] && maps[nx][ny] == 1){
-                    queue.offer(new int[]{nx,ny,distance + 1});
+                if(nx >= 0&& nx < n && ny >=0 && ny < m && !visited[nx][ny] && maps[nx][ny] == 1){// 조건에 맞다면 
+                    queue.offer(new int[]{nx,ny,distance + 1}); //큐에 집어넣고 방문처리
                     visited[nx][ny] = true;
                 }
             }
-            
-            
             
         }
         return -1;
