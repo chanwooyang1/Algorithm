11e24a0d1c22ec781ef901285ada5024811fb98e
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2024-12-18 13:57:41 +0900

    [level 3] Title: 단어 변환, Time: 0.24 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,30 +2,28 @@ import java.util.*;
 class Solution {
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-        if(!Arrays.asList(words).contains(target)){
+        if(!Arrays.asList(words).contains(target)){ // 만약 target이 들어있지 않다면 0 출력
             return 0;
         }
         
         boolean[] visited = new boolean[words.length];
         Queue<String> queue = new LinkedList<>();
-        
         queue.offer(begin);
         
         while(!queue.isEmpty()){
-            int size = queue.size();
+            int size = queue.size(); // queue가 int queue이면 항목을 추가해서 횟수를 계산할수 있지만 String queue이기 때문에 따로 루프를 주어서 횟수를 계산
             for(int i = 0; i < size; i++){
                 String current = queue.poll();
                 
-                if(current.equals(target)){
+                if(current.equals(target)){ //문제 풀때 까먹었던 최종 조건 설정
                     return answer;
                 }
                 
                 for(int j = 0; j < words.length; j++){
                     String other = words[j];
-                    if(check(current, other) && !visited[j]){
+                    if(check(current, other) && !visited[j]){ //조건 검사하고 방문하지 않았다면 큐에 삽입
                         queue.offer(other);
                         visited[j] = true;
-                        System.out.println(other);
                     }
                 }
             }answer++;
@@ -34,7 +32,7 @@ class Solution {
         
         return answer;
     }
-    private boolean check(String word, String other){
+    private boolean check(String word, String other){ // 단어 두개를 비교해서 다른 철자가 1개인지 체크
         int count = 0;
         for(int i = 0; i < word.length(); i++ ){
             if(count >= 2){
