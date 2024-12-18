7e6af76983a213747617a01ee61e679019ad9526
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2024-12-18 13:13:39 +0900

    [level 3] Title: 네트워크, Time: 0.24 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,24 +3,22 @@ class Solution {
     int answer = 0; 
     boolean[] visited;
     public int solution(int n, int[][] computers) {
-       visited = new boolean[n];
-        for(int i = 0; i < n; i++){
-            if(!visited[i]){
-                 dfs(computers, i);
-                answer++;
+       visited = new boolean[n]; //각각의 컴퓨터를 거쳤는지 확인하기 위해체크
+        for(int i = 0; i < n; i++){ //컴퓨터를 0번부터 돌면서
+            if(!visited[i]){  // 만약 거치지 않았다면
+                 dfs(computers, i); //로직 실행
+                answer++; // 끝까지 탐색후 돌아와서 네트워크 개수 추가
             }
         }
        
-        
         return answer;
     }
     private void dfs(int[][] computers, int index){
-        visited[index] = true;
-        int[] computer = computers[index];
-        for(int i = 0; i < computer.length; i++){
-            if(computer[i] == 1 && !visited[i]){
-                dfs(computers, i);
+        visited[index] = true; // 방문처리하고
+        for(int i = 0; i < computers.length; i++){ //해당 컴퓨터랑 연결된 컴퓨터 조사
+            if(computers[index][i] == 1 && !visited[i]){ //연결되어있으면서 방문하지 않았다면
+                dfs(computers, i); // 한번 더 dfs 들어가기
             }
         }
     }
-}
+} 
