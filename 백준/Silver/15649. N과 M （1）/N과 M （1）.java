62d51c58fe4d5660eeb361b50d367b1bbe9e8f18
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2025-01-09 15:08:53 +0900

    [Silver III] Title: N과 M (1), Time: 288 ms, Memory: 22716 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,7 +1,9 @@
 import java.io.*;
 public class Main{
+    public static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         String[] numbers = br.readLine().split(" ");
         int N = Integer.parseInt(numbers[0]);
         int M = Integer.parseInt(numbers[1]);
@@ -10,15 +12,17 @@ public class Main{
         boolean[] visited = new boolean[N];
         
         dfs(arr,visited, N, M, 0);
+        System.out.println(sb.toString());
         
         
     }
     public static void dfs(int[] arr, boolean[] visited, int N, int M, int depth){
         if(depth == M){
             for(int num : arr){
-                System.out.print(num + " ");
+               
+                sb.append(num).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
             return;
         }
         
