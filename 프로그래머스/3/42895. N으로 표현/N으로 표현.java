b5a772f4506fcdac14dd5ecdfaaaebf9144330d0
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2025-01-10 15:31:37 +0900

    [level 3] Title: N으로 표현, Time: 22.32 ms, Memory: 99.7 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,35 +1,33 @@
 import java.util.*;
 class Solution {
     public int solution(int N, int number) {
-    Set<Integer>[] dp = new HashSet[9];
+        int answer = 0;
+        Set<Integer>[] arr =  new HashSet[9];
         
-    for(int i = 1; i <= 8; i++){
-        dp[i] = new HashSet<>();
-        dp[i].add(Integer.parseInt(String.valueOf(N).repeat(i)));
-    }
-
-    for(int i = 1; i <= 8; i++){
-        for(int j = 1; j < i; j++){
-            for(int a : dp[j]){
-                for(int b : dp[i - j]){
-                    dp[i].add(a + b);
-                    dp[i].add(a - b);
-                    dp[i].add(a * b);
+        for(int i = 1; i<= 8; i++){
+            arr[i] = new HashSet<>();
+            arr[i].add(Integer.valueOf(String.valueOf(N).repeat(i)));
+        }
+       
+        for(int i = 1; i <= 8; i++){
+            for(int j = 1; j < i; j++){
+                for(int a : arr[j]){
+                    for(int b : arr[i - j]){
+                    arr[i].add(a + b);
+                    arr[i].add(a - b);
+                    arr[i].add(a * b);
                     if(b != 0){
-                        dp[i].add(a / b);
-                    }                    
+                        arr[i].add(a / b);
+                    }
+                    }
                 }
+            }
+            if(arr[i].contains(number)){
+                return i;
             }
         }
         
-        if(dp[i].contains(number)){
-            return i;
-        }
-    }
-        
-        
-        
-        
+       
         return -1;
     }
 }
