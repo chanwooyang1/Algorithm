60c4465fc9f9bc218c10887a963a8c23eae42345
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2025-02-19 22:23:17 +0900

    [Silver V] Title: 뒤집기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,19 +1,12 @@
 import sys
-input = sys.stdin.readline  # 빠른 입력을 위해 사용
+input = sys.stdin.readline
+a = list(input().strip())
+count = 0
 
-count1 = 0
-
-# 문자열 입력받기
-input_str = input().strip()  # 개행 문자 제거
-length = len(input_str)
-
-# 인접한 문자 비교
-for i in range(length - 1):
-    current = input_str[i]
-    next_char = input_str[i + 1]
+for i in range(len(a) - 1):
+    current = a[i]
+    next_value = a[i + 1]
     
-    if current != next_char:
-        count1 += 1
-
-# 결과 출력 (정수 나눗셈 사용)
-print((count1 + 1) // 2)
+    if current != next_value:
+        count += 1
+print((count+1) // 2)    
