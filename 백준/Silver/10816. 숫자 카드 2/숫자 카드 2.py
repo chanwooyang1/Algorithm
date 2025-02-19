6cc53e4b63d859b2d02ce407739798401186f71f
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2025-02-19 21:37:02 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 576 ms, Memory: 254508 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,13 +1,14 @@
 import sys
 input = sys.stdin.readline
-N = int(input())
-a = map(int, input().split())
 
-hash = {}
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+b = list(map(int, input().split()))
+
+dict = {}
+
 for num in a:
-    hash[num] = hash.setdefault(num, 0 ) + 1
-    
-M = int(input())
-b = map(int, input().split())
+    dict[num] = dict.setdefault(num, 0) + 1
 for num in b:
-    print(hash.setdefault(num,0), end = ' ')
+    print(dict.setdefault(num, 0), end = " ")
