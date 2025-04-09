a2e6f291c7c88a392f6fac3bd71bf9667126445d
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2025-04-09 09:48:06 +0900

    [Gold V] Title: 동전 2, Time: 300 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -2,15 +2,12 @@ n, k = map(int, input().split())
 
 coins =[int(input()) for _ in range(n)]
 
-dp = [-1] * (k + 1)
+dp = [k + 1] * (k + 1)
 dp[0] = 0
 
 for coin in coins:
-    for i in range(coin, k+1):
-        if dp[i- coin] != -1:
-            if dp[i] == -1:
-                dp[i] = dp[i-coin] + 1
-            else:
-                dp[i] = min(dp[i], dp[i-coin] + 1)
-print(dp[k])
+    for i in range(coin, k + 1):
+        dp[i] = min(dp[i], dp[i - coin] + 1)
+
+print(dp[k] if dp[k] != k + 1 else -1)
 
