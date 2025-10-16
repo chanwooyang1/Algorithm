89897bdbeb8295bffdf5eed813779c773a402cb7
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2025-10-16 21:02:47 +0900

    [level 2] Title: N-Queen, Time: 1508.42 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -1,20 +1,31 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
 def solution(n):
-    answer = 0
-    # graph = [[False] * n for _ in range(n)]  # 사실 안 써도 됨
-    cols = [False] * n          # 열 체크
-    diag1 = [False] * (2*n-1)   # 좌상-우하 대각선
-    diag2 = [False] * (2*n-1)   # 우상-좌하 대각선
-
-    def dfs(row):
-        nonlocal answer
-        if row == n:
-            answer += 1
-            return
-        for col in range(n):
-            if not cols[col] and not diag1[row-col+n-1] and not diag2[row+col]:
-                cols[col] = diag1[row-col+n-1] = diag2[row+col] = True
-                dfs(row+1)
-                cols[col] = diag1[row-col+n-1] = diag2[row+col] = False  # 백트래킹
-
-    dfs(0)
+    check_col = [False] * 100; check_d1 = [False] * 100; check_d2 = [False] * 100
+    def process(row):
+        answer = 0
+        if row == n+1:
+            return 1
+        for i in range(1,n+1):
+            d1 = row+i; d2 = n + (row - i)
+            if check_col[i] == False and check_d1[d1] == False and check_d2[d2] == False:
+                check_col[i] = True; check_d1[d1] = True; check_d2[d2] = True
+                answer += process(row+1)
+                check_col[i] = False; check_d1[d1] = False; check_d2[d2] = False
+        return answer
+    answer = process(1)
     return answer
