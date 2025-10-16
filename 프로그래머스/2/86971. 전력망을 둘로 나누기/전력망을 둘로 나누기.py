1af01eb0dd7b519bfcf79cfb063ea8821f4c2962
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2025-10-16 15:00:55 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.89 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,32 +1,26 @@
 def solution(n, wires):
     answer = n
-    adj = [[] for _ in range(n + 1)]
-    
+    adj = [[] for _ in range(n+1)]
     for v1, v2 in wires:
         adj[v1].append(v2)
         adj[v2].append(v1)
-        
+    
+    
+    
     for v1, v2 in wires:
-        adj[v1].remove(v2)
-        adj[v2].remove(v1)
-        
-        visited = [False] * (n+1)
-        size = dfs(v1, visited, adj)
-        diff = abs(size - (n - size))
+        visited = [False] * (n + 1)
+        have = dfs(v1,adj, visited,  (v1,v2))
+        diff = abs(have - (n - have))
         answer = min(diff, answer)
-        
-        adj[v1].append(v2)
-        adj[v2].append(v1)
     
-   
     
     return answer
-
-def dfs(node, visited, adj):
-    count = 0
+def dfs(node,adj, visited, cut):
     visited[node] = True
-    count += 1
+    count = 1
     for next_node in adj[node]:
-        if not visited[next_node]:
-            count += dfs(next_node, visited, adj)
+        if not visited[next_node] and cut != (node, next_node) and cut != (next_node, node):
+            count += dfs(next_node,adj, visited, cut)
     return count
+    
+    
