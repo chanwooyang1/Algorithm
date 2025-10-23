10c466ad8bd35e7b842bb1db9007e1edd1638695
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2025-10-23 15:37:30 +0900

    [Silver III] Title: N과 M (1), Time: 112 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,19 +1,19 @@
-
 N, M = map(int, input().split())
-
-visited = [False] * (N + 1)
-path = []
-def dfs():
-    if len(path) == M:
-        print(" ".join(map(str, path)))
+numbers = [i for i in range(1, N+1)]
+visited = [False] * (N+1)
+def sequence(numbers, visited, M, answer):
+    if len(answer) == M:
+        print(" ".join(map(str, answer)))
         return
-    for i in range(1, N +1):
-        if not visited[i]:
-            visited[i] = True
-            path.append(i)
-            dfs()
-            path.pop()
-            visited[i] = False
-
-dfs()
-
+    for number in numbers:
+        if not visited[number]:
+            answer.append(number)
+            visited[number] = True
+            sequence(numbers, visited, M, answer)
+            answer.pop()
+            visited[number] = False
+sequence(numbers, visited, M, [])    
+    
+    
+    
+    
