27a4172b92eee060d336786329ac6bcfe215196c
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2025-10-23 15:52:35 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,5 +1,4 @@
 N, M = map(int, input().split())
-visited = [False] * (N+1)
 answer = []
 def dfs(start):
     if len(answer) == M:
@@ -7,10 +6,8 @@ def dfs(start):
         return
         
     for i in range(start, N+1):
-        if not visited[i]:
-            answer.append(i)
-            visited[i] = True
-            dfs(i+1)
-            answer.pop()
-            visited[i] = False
+        answer.append(i)
+        dfs(i+1)
+        answer.pop()
+            
 dfs(1)
