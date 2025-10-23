667c9d8f03aaceaf1c02a13d7974b17d79630e69
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2025-10-23 17:03:37 +0900

    [level 2] Title: 피로도, Time: 35.67 ms, Memory: 9.04 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,18 +1,19 @@
-answer = 0
-
-def dfs(stamina, count,n,visited, dungeons):
-    global answer
-    answer = max(answer, count)
-    for i in range(n):
-        if not visited[i] and stamina >= dungeons[i][0]:
-            visited[i] = True
-            dfs(stamina - dungeons[i][1], count + 1, n, visited, dungeons)
-            visited[i] = False  # 백트래킹
-
 def solution(k, dungeons):
-    n = len(dungeons)
-    visited = [False] * n
-    # global answer
-    # answer = 0
-    dfs(k, 0,n,visited, dungeons)
+    answer = -1
+    l = len(dungeons)
+    visited = [False] * l
+    
+    def dfs(k, count):
+        nonlocal answer
+        
+        if count > answer:
+            answer = count
+            
+        for i in range(l):
+            if not visited[i] and k >= dungeons[i][0]:
+                visited[i] = True
+                dfs(k - dungeons[i][1], count + 1)
+                visited[i] = False
+    dfs(k,0 )      
+    
     return answer
