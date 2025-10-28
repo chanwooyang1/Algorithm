b95a4fa74ac8962e30759b7cafb584bbfe0e6a25
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2025-10-28 16:45:02 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 35680 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,44 +1,43 @@
 import sys
 from collections import deque
-
-input=sys.stdin.readline
-
-n,m,start=map(int,input().split())
-checked=[False]*(n+1)
-
-graph=[[] for _ in range(n+1)]
+input = sys.stdin.readline
+N, M , V = map(int, input().split())
+adj_list = [[] for _ in range(N + 1)]
 
 
-for _ in range(m):
-    a,b=map(int,input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-
-for i in range(len(graph)):
-    graph[i].sort()
-
-def dfs(start):
-    print(start,end=' ')
-    checked[start]=True
-    for i in graph[start]:
-        if not checked[i]:
-            dfs(i)
-            
-
-def bfs(start):
-    q=deque([start])
-    checked[start]=True
-    while q:
-        now=q.popleft()
-        print(now,end=' ')
-        for i in graph[now]:
-            if not checked[i]:
-                q.append(i)
-                checked[i] = True
-dfs(start)
-checked=[False]*(n+1)
-print()
-bfs(start)
+for _ in range(M):
+    a, b = map(int, input().split())
+    adj_list[a].append(b)
+    adj_list[b].append(a)
 
     
+for i in range(1, N+1):
+    adj_list[i].sort()
+
+    
+def dfs(node, visited):
+    visited[node] = True
+    print(node, end = " ")
+    for next_node in adj_list[node]:
+        if not visited[next_node]:
+            dfs(next_node, visited)
+
+            
+def bfs(start):
+    queue = deque()
+    queue.append(start)
+    visited = [False] * (N+1)
+    visited[start] = True
+    
+    while queue:
+        node = queue.popleft()
+        print(node, end= " ")
+        for next_node in adj_list[node]:
+            if not visited[next_node]:
+                queue.append(next_node)
+                visited[next_node] = True
+dfs_visited = [False] * (N+1)
+dfs(V, dfs_visited)
+print()
+bfs(V)
     
