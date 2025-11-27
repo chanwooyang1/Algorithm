0282a042ad1559f07eba864b4b62e4e068444864
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2025-11-27 22:32:38 +0900

    [level 2] Title: 타겟 넘버, Time: 330.46 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 9.06 MB, 시간: 416.58 ms
+메모리: 9.18 MB, 시간: 330.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 21일 14:15:07
+2025년 11월 27일 22:32:34
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,17 +1,25 @@
+import sys
+sys.setrecursionlimit(10**6)
+
 def solution(numbers, target):
     answer = 0
-     
-    def dfs(index, current):
-        nonlocal answer
+    n = len(numbers) # 길이를 미리 저장
+    
+    # 함수 안에 함수를 만들면 외부 변수(numbers, target)를 그냥 쓸 수 있음
+    def dfs(idx, current_sum):
+        nonlocal answer # solution의 answer 변수를 갖다 쓰겠다는 뜻
         
-        if index == (len(numbers)):
-            if  current == target:
+        # 1. 탈출 조건: 끝까지 다 탐색했을 때
+        if idx == n:
+            if current_sum == target:
                 answer += 1
-                return 
-            return
-        dfs(index + 1, current + numbers[index])
-        dfs(index + 1, current - numbers[index])
+            return # 여기서만 리턴해야 함!
         
-        
-    dfs(0 , 0)        
+        # 2. 수행 동작: 더하거나 빼거나 (재귀)
+        dfs(idx + 1, current_sum + numbers[idx])
+        dfs(idx + 1, current_sum - numbers[idx])
+
+    # DFS 시작 (인덱스 0, 합계 0)
+    dfs(0, 0)
+    
     return answer
