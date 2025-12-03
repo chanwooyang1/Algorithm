ca3159ff2c67f002ded20395fb3912e6d36ede6f
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2025-12-03 20:18:52 +0900

    [level 0] Title: 수 조작하기 1, Time: 8.32 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -0,0 +1,12 @@
+def solution(n, control):
+    answer = n
+    for char in control:
+        if char == "w":
+            answer += 1
+        elif char == "s":
+            answer -= 1
+        elif char == "d":
+            answer += 10
+        else:
+            answer -= 10
+    return answer
