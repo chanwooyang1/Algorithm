9e3d49beee84c2ba4b77eed6dde25aacd9116dcc
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2025-12-03 20:11:48 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.09 ms, Memory: 9.89 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -1,9 +1,5 @@
 def solution(n):
-    answer = [[] * n for _ in range(n)]
+    answer = [[0] * n for _ in range(n)]
     for i in range(n):
-        for j in range(n):
-            if i == j:
-                answer[i].append(1)
-            else:
-                answer[i].append(0)
+        answer[i][i] = 1
     return answer
