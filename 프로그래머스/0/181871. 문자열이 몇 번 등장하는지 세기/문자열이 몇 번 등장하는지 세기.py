3e29526271817fea63cc5606daf89b01c9328d49
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2025-12-08 15:36:18 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.34 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
@@ -1,10 +1,8 @@
 def solution(myString, pat):
     answer = 0
-    pointer = 0
-    while True:
-        idx = myString.find(pat, pointer)
-        if idx == -1:
-            break
-        answer += 1
-        pointer = idx + 1
+    l = len(myString)
+    
+    for i in range(l):
+        if myString[i:].startswith(pat):
+            answer += 1
     return answer
