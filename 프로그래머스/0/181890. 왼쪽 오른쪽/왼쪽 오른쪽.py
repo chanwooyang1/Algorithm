4f35b996e41f0c16b6581364b755c5e812546c5f
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2025-12-09 15:42:50 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -1,16 +1,7 @@
 def solution(str_list):
-    answer = "".join(str_list)
-    l = answer.find("l")
-    r = answer.find("r")
-    
-    if 0 <= l < r and r != -1:
-        return list(answer[:l])
-    elif 0 <= r < l and l != -1:
-        return list(answer[r+1:])
-    elif r < 0 <= l:
-        return list(answer[:l])
-    elif l <0 <= r:
-        return list(answer[r+1:])
-    elif l < 0 and r < 0:
-        return []
-        
+    for idx, val in enumerate(str_list):
+        if str_list[idx] == "l":
+            return str_list[:idx]
+        elif str_list[idx] == "r":
+            return str_list[idx+1:]
+    return []
