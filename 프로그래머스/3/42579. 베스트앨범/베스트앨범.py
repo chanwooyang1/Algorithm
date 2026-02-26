0f10ea321f300e3536463e4b577e2fb6dfbd1b3c
chanwoo yang <44763490+chanwooyang1@users.noreply.github.com>
2026-02-26 16:18:52 +0900

    [level 3] Title: 베스트앨범, Time: 0.09 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -9,26 +9,24 @@ def count_total(song_list): # 장르당 총 횟수 list-> int
 def solution(genres, plays):
     answer = []
     songs = defaultdict(list)
-    idx = 0
     
-    for genre, play in zip(genres, plays):
+    for idx, (genre, play) in enumerate(zip(genres, plays)):
         songs[genre].append((idx, play))
-        idx += 1
-        
-    song_with_count = []
-    for key, value in songs.items():
-        count = count_total(value)
-        song_with_count.append((count, key)) 
-    song_with_count.sort(reverse = True)
     
-    for count, genre in song_with_count:
+    
+    genre_order = sorted(((sum(play for _, play in lst), genre) for genre, lst in songs.items()), reverse = True)
+   
+    for _, genre in genre_order:
         genre_list = songs[genre]
-        if len(genre_list) == 1:
-            answer.append(genre_list.pop()[0])
-            continue
-        genre_list.sort(key = lambda x: (-x[1], x[0]))
+        genre_list.sort(key=lambda x : (-x[1], x[0]))
+        answer.extend([idx for idx, _ in genre_list[:2]])
         
-        for i in range(2):
-            answer.append(genre_list[i][0])
+        # if len(genre_list) == 1:
+        #     answer.append(genre_list[0][0])
+        #     continue
+        # genre_list.sort(key = lambda x: (-x[1], x[0]))
+        
+        # for i in range(2):
+        #     answer.append(genre_list[i][0])
    
     return answer
